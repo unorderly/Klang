99ab0cd0a8cf8c861a47a28361a5aa0173f53567
Mariela <mariela.castillo.mc2@gmail.com>
2025-02-19 13:59:54 +0100

    WIP: stopping sound when tapped again in widget

--- App/IntentRunner.swift
+++ App/IntentRunner.swift
@@ -11,19 +11,29 @@ import UIKit
 import MediaPlayer
 
 enum IntentRunner {
+    private static var player: AudioPlayer?
+
     static func perform(intent: SoundIntent) async throws -> some IntentResult {
+        if let currentPlayer = player, currentPlayer.isPlaying {
+            print("Stopping sound \(intent.sound.title)")
+            currentPlayer.stop()
+            player = nil
+            return .result()
+        }
+
         print("Start playing \(intent.sound.title)")
-        let player = try! AudioPlayer(url: intent.sound.file.fileURL!)
+        let newPlayer = try! AudioPlayer(url: intent.sound.file.fileURL!)
+        player = newPlayer
 
         print("Created sound for \(intent.sound.title)")
 
-        if intent.isFullBlast && player.isOnSpeaker {
+        if intent.isFullBlast && newPlayer.isOnSpeaker {
             await MPVolumeView.setVolume(1)
         }
 
         do {
             print("Playing sound \(intent.sound.title)")
-            try await player.playOnQueue()
+            try await newPlayer.playOnQueue()
             print("Sound \(intent.sound.title) stopped")
         } catch {
             print("Playing Sound fauled error:", error.localizedDescription)
