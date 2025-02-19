39e80d721a69a638b9b3903046b58b30e0909c15
Mariela <mariela.castillo.mc2@gmail.com>
2025-02-19 15:04:03 +0100

    Use sound id instead of title

--- App/IntentRunner.swift
+++ App/IntentRunner.swift
@@ -11,20 +11,23 @@ import UIKit
 import MediaPlayer
 
 enum IntentRunner {
-    private static var activePlayer: [String: AudioPlayer] = [:]
+    private static var activePlayer: [UUID: AudioPlayer] = [:]
 
     static func perform(intent: SoundIntent) async throws -> some IntentResult {
-        let soundTitle = intent.sound.title
+        let soundID = intent.sound.id
 
-        if let player = activePlayer[soundTitle], player.isPlaying {
+        if let player = activePlayer[soundID], player.isPlaying {
             print("Stopping sound \(intent.sound.title)")
             player.stop()
-            activePlayer[soundTitle] = nil
+            activePlayer[soundID] = nil
             return .result()
         }
 
+        guard let fileURL = intent.sound.file.fileURL, let newPlayer = try? AudioPlayer(url: fileURL) else {
+            print("Failed to create player for \(intent.sound.title)")
+            return .result()
+        }
         print("Start playing \(intent.sound.title)")
-        let newPlayer = try! AudioPlayer(url: intent.sound.file.fileURL!)
 
         print("Created sound for \(intent.sound.title)")
 
@@ -32,17 +35,17 @@ enum IntentRunner {
             await MPVolumeView.setVolume(1)
         }
 
-        activePlayer[soundTitle] = newPlayer
+        activePlayer[soundID] = newPlayer
 
         do {
             print("Playing sound \(intent.sound.title)")
             try await newPlayer.playOnQueue()
             print("Sound \(intent.sound.title) stopped")
 
-            activePlayer[intent.sound.title] = nil
+            activePlayer[intent.sound.id] = nil
         } catch {
             print("Playing Sound failed error:", error.localizedDescription)
-            activePlayer[intent.sound.title] = nil
+            activePlayer[intent.sound.id] = nil
         }
         return .result()
     }
