df92b026b8965c080a7374ec621610d1673a98b0
Mariela <mariela.castillo.mc2@gmail.com>
2025-02-19 14:27:39 +0100

    Playing more than one sound, stopping last one

--- App/IntentRunner.swift
+++ App/IntentRunner.swift
@@ -11,19 +11,20 @@ import UIKit
 import MediaPlayer
 
 enum IntentRunner {
-    private static var player: AudioPlayer?
+    private static var currentPlayer: AudioPlayer?
+    private static var currentSoundTitle: String?
 
     static func perform(intent: SoundIntent) async throws -> some IntentResult {
-        if let currentPlayer = player, currentPlayer.isPlaying {
+        if let player = currentPlayer, player.isPlaying, currentSoundTitle == intent.sound.title {
             print("Stopping sound \(intent.sound.title)")
-            currentPlayer.stop()
-            player = nil
+            player.stop()
+            currentPlayer = nil
+            currentSoundTitle = nil
             return .result()
         }
 
         print("Start playing \(intent.sound.title)")
         let newPlayer = try! AudioPlayer(url: intent.sound.file.fileURL!)
-        player = newPlayer
 
         print("Created sound for \(intent.sound.title)")
 
@@ -31,12 +32,22 @@ enum IntentRunner {
             await MPVolumeView.setVolume(1)
         }
 
+        currentPlayer = newPlayer
+        currentSoundTitle = intent.sound.title
+
         do {
             print("Playing sound \(intent.sound.title)")
             try await newPlayer.playOnQueue()
             print("Sound \(intent.sound.title) stopped")
+
+            if currentSoundTitle == intent.sound.title {
+                currentPlayer = nil
+                currentSoundTitle = nil
+            }
         } catch {
-            print("Playing Sound fauled error:", error.localizedDescription)
+            print("Playing Sound failed error:", error.localizedDescription)
+            currentPlayer = nil
+            currentSoundTitle = nil
         }
         return .result()
     }
